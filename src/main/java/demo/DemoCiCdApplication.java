dacffa5a10b8b47b5c212efd8233adcf6413df7c
BARRY <barry.souleymane88@yahoo.com>
2025-11-12 15:56:40 +0100

    first commit

---
+++ src/main/java/demo/DemoCiCdApplication.java
@@ -0,0 +1,22 @@
+package demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoCiCdApplication {
+
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to my CI/CD ";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCiCdApplication.class, args);
+    }
+
+}
